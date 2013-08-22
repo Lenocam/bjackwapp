087a333b27a61e90842589b37d5ae0937051a21b
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-22 14:29:33 -0400

    Added calculate, deal, player's name in titles etc...

--- main.rb
+++ main.rb
@@ -4,8 +4,35 @@ require 'sinatra/reloader'
 
 set :sessions, true
 
+helpers do
+
+def calculate(cards)
+  face_values = cards.map{|e| e[1]}
+
+    total = 0
+    face_values.each do |value|
+      if value == "A"
+        total += 11
+      elsif (2..10).include?(value.to_i) 
+        total += value.to_i
+      else 
+        total += 10
+      end
+    end
+
+    #Aces are systematic
+    face_values.select { |e| e == 'A'}.count.times do
+        break if total <= 21
+        total -= 10 
+    end
+
+    total
+  end  
+end
+
+
 get '/' do
-  if session[:player_name]
+  if session[:new_player]
   
   else 
     redirect '/new_player'
@@ -18,5 +45,29 @@ get '/new_player' do
 end
 
 post '/new_player' do
-  session[:player_name] = params[:player_name]
+  session[:new_player] = params[:new_player]
+  redirect '/game'
+end
+
+get '/game' do
+
+  #deck 
+  suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+  values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+  session[:deck] = suits.product(values).shuffle!
+  
+  
+#dealer cards
+session[:dealer_cards] = []
+
+#player cards
+session[:player_cards] = []
+
+#deal cards
+session[:player_cards] << session[:deck].pop
+session[:dealer_cards] << session[:deck].pop
+session[:player_cards] << session[:deck].pop
+session[:dealer_cards] << session[:deck].pop
+
+  erb :game
 end
